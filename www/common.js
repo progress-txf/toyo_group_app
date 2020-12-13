d3ba9131967cc90f8309de9c30713c7e0487ad1d
txf72 <progresso7272@icloud.com>
2020-12-13 11:11:28 +0900

    Project's Initial Commit

---
+++ www/common.js
@@ -0,0 +1,48 @@
+// ============================================================================
+// 
+// common.js
+// 
+// 汎用的なクラスの定義を記述したファイルです。
+// このファイルには、以下のクラスが含まれます。
+// 
+// ・Pointクラス
+// 
+// ============================================================================
+
+// - point --------------------------------------------------------------------
+function Point(){
+	this.x = 0;
+	this.y = 0;
+}
+
+Point.prototype.distance = function(p){
+	var q = new Point();
+	q.x = p.x - this.x;
+	q.y = p.y - this.y;
+	return q;
+};
+
+Point.prototype.length = function(){
+	return Math.sqrt(this.x * this.x + this.y * this.y);
+};
+
+Point.prototype.normalize = function(){
+	var i = this.length();
+	if(i > 0){
+		var j = 1 / i;
+		this.x *= j;
+		this.y *= j;
+	}
+};
+
+Point.prototype.rotate = function(radian){
+	var sin = Math.sin(radian);
+	var cos = Math.cos(radian);
+	var x = this.x * cos - this.y * sin;
+	var y = this.x * sin + this.y * cos;
+	this.x = x;
+	this.y = y;
+};
+
+
+
